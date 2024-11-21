3b0f770d047654057b7a5c909ca326d7f0b11d37
Finnawk <stephenhark19@gmail.com>
2024-11-20 22:17:16 -0600

    Code changes to Calculator

--- calculator.py
+++ calculator.py
@@ -1,85 +1,79 @@
-# We will be making a calculator that
-# says hello.
-
-# If the user type in 07734 into the calculator
-# a window will pop up that will say hello to the
-# user.
-
 import tkinter as tk
 
+calculation = ""
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
+
+
+def evaluate_calcualtion():
+    global calculation
+    try:
+        calculation = str(eval((calculation)))
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except:
+        clear_field()
+        text_result.insert(1.0, "error")
+
+def clear_field():
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
+
+
+
 root = tk.Tk()
+root.geometry("300x275")
+root.title("Calculator Says Hello")
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
 
-root.geometry("300x500")
-root.title("Calculator")
-
-label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack(padx=20, pady=50)
-
-textbox = tk.Text(root, height= 3, font=('Arial', 10))
-textbox.pack(padx=10)
-
-# button = tk.Button(root, text="Click Me!", font=('Arial', 18))
-# button.pack(padx=10, pady=10)
-
-buttom_frame = tk.Frame(root)
-buttom_frame.columnconfigure(0, weight=1)
-buttom_frame.columnconfigure(1, weight=1)
-buttom_frame.columnconfigure(2, weight=1)
-buttom_frame.columnconfigure(3, weight=1)
-buttom_frame.columnconfigure(4, weight=1)
-
-def text():
-    textbox.insert(0, "1")
-
-
-btn1 = tk.Button(buttom_frame, text="1", font=('arial', 18), command=text)
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
-
-btn2 = tk.Button(buttom_frame, text="2", font=('arial', 18))
-btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
-
-btn3 = tk.Button(buttom_frame, text="3", font=('arial', 18))
-btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
-
-btn_divide = tk.Button(buttom_frame, text="/", font=('arial', 18))
-btn_divide.grid(row=0, column=3, sticky=tk.W+tk.E)
-
-btn4 = tk.Button(buttom_frame, text="4", font=('arial', 18))
-btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
-
-btn5 = tk.Button(buttom_frame, text="5", font=('arial', 18))
-btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
-
-btn6 = tk.Button(buttom_frame, text="6", font=('arial', 18))
-btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
-
-btn_multiply = tk.Button(buttom_frame, text="*", font=('arial', 18))
-btn_multiply.grid(row=1, column=3, sticky=tk.W+tk.E)
-
-btn7 = tk.Button(buttom_frame, text="7", font=('arial', 18))
-btn7.grid(row=2, column=0, sticky=tk.W+tk.E)
-
-btn8 = tk.Button(buttom_frame, text="8", font=('arial', 18))
-btn8.grid(row=2, column=1, sticky=tk.W+tk.E)
-
-btn9 = tk.Button(buttom_frame, text="9", font=('arial', 18))
-btn9.grid(row=2, column=2, sticky=tk.W+tk.E)
-
-btn_add = tk.Button(buttom_frame, text="+", font=('arial', 18))
-btn_add.grid(row=2, column=3, sticky=tk.W+tk.E)
-
-btn_clear = tk.Button(buttom_frame, text="C", font=('arial', 18))
-btn_clear.grid(row=3, column=0, sticky=tk.W+tk.E)
-
-btn_clear = tk.Button(buttom_frame, text="0", font=('arial', 18))
-btn_clear.grid(row=3, column=2, sticky=tk.W+tk.E)
-
-btn_minus = tk.Button(buttom_frame, text="-", font=('arial', 18))
-btn_minus.grid(row=3, column=3, sticky=tk.W+tk.E)
-
-buttom_frame.pack(fill='x')
-
+btn1 =tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+btn1.grid(row=2, column=1)
+btn2 =tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial", 14))
+btn2.grid(row=2, column=2)
+btn3 =tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial", 14))
+btn3.grid(row=2, column=3)
+btn4 =tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font=("Arial", 14))
+btn4.grid(row=3, column=1)
+btn5 =tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font=("Arial", 14))
+btn5.grid(row=3, column=2)
+btn6 =tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, font=("Arial", 14))
+btn6.grid(row=3, column=3)
+btn7 =tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, font=("Arial", 14))
+btn7.grid(row=4, column=1)
+btn8 =tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font=("Arial", 14))
+btn8.grid(row=4, column=2)
+btn9 =tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial", 14))
+btn9.grid(row=4, column=3)
+btn0 =tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font=("Arial", 14))
+btn0.grid(row=5, column=2)
+btn_equal =tk.Button(root, text="=", command=evaluate_calcualtion, width=5, font=("Arial", 14))
+btn_equal.grid(row=6, column=1)
+btn_plus =tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14))
+btn_plus.grid(row=2, column=4)
+btn_minus =tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14))
+btn_minus.grid(row=3, column=4)
+btn_times =tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
+btn_times.grid(row=4, column=4)
+btn_divide =tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14))
+btn_divide.grid(row=5, column=4)
+btn_period =tk.Button(root, text=".", command=lambda: add_to_calculation("."), width=5, font=("Arial", 14))
+btn_period.grid(row=6, column=2)
+btn_open =tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial", 14))
+btn_open.grid(row=5, column=3)
+btn_close =tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial", 14))
+btn_close.grid(row=5, column=1)
+btn_clear =tk.Button(root, text="C", command=clear_field, width=5, font=("Arial", 14))
+btn_clear.grid(row=6, column=3)
 root.mainloop()
+
+
+
+
 
 
 
